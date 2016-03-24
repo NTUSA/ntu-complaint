d33a589163ab0fce7bd3303466837cc7ecd04fdc
Poren Chiang <ren.chiang@gmail.com>
2016-03-24 12:38:47 +0800

    Start implement parsing algorithm

--- fetch.py
+++ fetch.py
@@ -9,6 +9,27 @@ def fetch(number, cookie):
     request.encoding = 'big5'
     return request.text
 
+def search_one(pattern, string):
+    match = re.search(pattern, string)
+    if match is not None:
+        match = match.group(1)
+    return match
+
+def parse(text):
+    if r'<td width="100%">無此篇建議案</td>' in text:
+        return { 'error': 'not_found' }
+    elif r'<td width="100%">無權調閱此篇建議案</td>' in text:
+        return { 'error': 'unauthorized' }
+
+    number = search_one(r'金玉集</a> \| 第 (\d+) 則建言', text)
+    if not number:
+        stderr.write('Cannot parse text properly, see if cookie expired')
+        raise Exception('Parse failed')
+    
+    return {
+        'number': number
+    }
+
 try:
     with open('cookie.json', 'r') as f:
         cookie = json.load(f)
@@ -16,4 +37,4 @@ except IOError:
     stderr.write('Cookie file not found')
     exit(-1)
 
-print(fetch(8845, cookie=cookie))
+print(parse(fetch(8845, cookie=cookie)))
